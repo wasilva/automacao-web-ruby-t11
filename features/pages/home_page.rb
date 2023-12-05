a13336cef320dca641d345925ec9e96d31af4b5a
wasilva <contato.wasilva@hotmail.com>
2023-12-04 21:37:36 -0300

    ajuste metodos

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -11,33 +11,27 @@ class Home < SitePrism::Page
 	#VALIDAÇÃO
 	element :titulo_sobre_nos, :xpath, '//p[text()="Sobre nós"]'
 	element :titulo_servicos, :xpath, '//h3[text()="Soluções Personalizadas"]'
-	
 
 	def click_button_menu(menu_item)
 
 		case menu_item
-		when "Sobre"
+		when menu_item
 			menu_item_sobre.click
-		when "Serviços"
+		when menu_item
 			menu_item_servicos.click
 		else
 			raise "Elemento não encontrado: #{menu_item}."
 		end
 	end
 
-
 	def validation_menu(sessao)
 		case sessao
-		when "Sobre nós"
+		when sessao
 			expect(titulo_sobre_nos).to have_text(sessao)
-		when "Serviços"
+		when sessao
 			expect(titulo_servicos).to have_text(sessao)
 		else
 			raise "Elemento não encontrado: #{menu_item}."
 		end
-
 	end
-
-
-
 end
